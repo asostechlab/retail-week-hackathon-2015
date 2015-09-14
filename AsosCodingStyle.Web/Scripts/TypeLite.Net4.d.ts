6a213fd60e85a6b812a9bd463af95e67b70e34f3
massimoahmadi <info@massimoahmadi.com>
2015-09-14 23:31:28 +0100

    Add API example and C# to TypeScript model conversion

---
+++ AsosCodingStyle.Web/Scripts/TypeLite.Net4.d.ts
@@ -0,0 +1,27 @@
+﻿
+ 
+ 
+
+ 
+
+/// <reference path="Enums.ts" />
+
+declare module AsosCodingStyle.Web.Models {
+	interface EmployeeRecord {
+		Benefits: AsosCodingStyle.Web.Models.Benefit[];
+		Id: System.Guid;
+		Name: string;
+		Title: string;
+		Salary: number;
+	}
+	interface Benefit {
+		Type: AsosCodingStyle.Web.Models.BenefitType;
+		ValidUntil: Date;
+	}
+}
+declare module System {
+	interface Guid {
+	}
+}
+
+
