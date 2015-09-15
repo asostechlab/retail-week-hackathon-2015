792f4584368264cbcedc9aa586f694e3fc389856
Massimo Ahmadi <info@massimoahmadi.com>
2015-09-15 20:25:05 +0100

    Add WebApi project to setup-iis project

--- setup-iis.ps1
+++ setup-iis.ps1
@@ -25,3 +25,39 @@ else
 
     New-Website -Name AsosCodingStyle.Web -Force -Port 1214 -PhysicalPath "$currentDirectoryItem\AsosCodingStyle.Web" -ApplicationPool AsosCodingStyle.Web
 }
+
+
+
+
+
+
+
+
+
+
+
+if (Test-Path IIS:\AppPools\AsosCodingStyle.WebApi)
+{
+    Write-Host "IIS app pool AsosCodingStyle.WebApi already exists - skipping"
+}
+else
+{
+    Write-Host "Creating IIS app pool AsosCodingStyle.WebApi"
+    
+    New-WebAppPool AsosCodingStyle.WebApi
+    Set-ItemProperty IIS:\AppPools\AsosCodingStyle.WebApi managedRuntimeVersion v4.0
+}
+
+if (Test-Path IIS:\Sites\AsosCodingStyle.WebApi)
+{
+    Write-Host "IIS website already exists - skipping"
+}
+else
+{
+    Write-Host "Creating IIS site AsosCodingStyle.WebApi"
+
+    $currentFileItem = (Get-Variable MyInvocation).Value.MyCommand.Path | Get-Item
+    $currentDirectoryItem =  Get-Item $currentFileItem.DirectoryName
+
+    New-Website -Name AsosCodingStyle.WebApi -Force -Port 1215 -PhysicalPath "$currentDirectoryItem\AsosCodingStyle.WebApi" -ApplicationPool AsosCodingStyle.WebApi
+}
