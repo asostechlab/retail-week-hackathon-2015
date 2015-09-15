b0f0cc83c11089234dea51c078864926209968aa
Dylan Morley <dylanm@asos.com>
2015-09-16 00:19:27 +0100

    Refactor - get\save order

--- AsosCodingStyle.WebApi/Controllers/OrdersController.cs
+++ AsosCodingStyle.WebApi/Controllers/OrdersController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Web.Http;
-using AsosCodingStyle.Data;
-using AsosCodingStyle.DataAccess;
-
-namespace AsosCodingStyle.WebApi.Controllers
+﻿namespace AsosCodingStyle.WebApi.Controllers
 {
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Data;
+    using DataAccess;
+
     [RoutePrefix("customer/orders")]
     public class OrdersController : ApiController
     {
@@ -14,40 +12,14 @@ namespace AsosCodingStyle.WebApi.Controllers
         [Route("{orderId}")]
         public async Task<Order> Get(int orderId)
         {
-            // return await new OrderRepository().GetOrder(orderId.ToString()); ;
-
-            var orderItems = new List<OrderItem>();
-            var products = Store.Products;
-            var prices = new List<double> { 69.99, 49.99, 90.00, 75.00, 39.99, 28.50 };
-
-            for (var i = 0; i < 6; i++)
-            {
-                orderItems.Add(new OrderItem
-                {
-                    OrderItemId = i,
-                    Product = products[i],
-                    Quantity = 1,
-                    Price = prices[new Random().Next(5)]
-                });
-            }
-
-            return new Order { Id = 1.ToString(), DateCreated = DateTime.UtcNow, Items = orderItems };
+            return await new OrderRepository().GetOrder(orderId.ToString());
         }
 
         [HttpPost]
         [Route("save")]
         public async Task Save([FromBody] Order order)
         {
-            try
-            {
-                await new OrderRepository().SaveOrder(order);
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debugger.Break();   
-            }
-            
+            await new OrderRepository().SaveOrder(order);
         }
-
     }
 }
