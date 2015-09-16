c8f088607f0fc9c044b8459668e70cd7cc4b9711
Nik Crabtree <nikc@asos.com>
2015-09-16 10:34:15 +0100

    Basic working app

---
+++ asos.codingstyle.android/app/src/main/java/com/asos/codingstyle/asoscodingstyle/MainActivity.java
@@ -0,0 +1,132 @@
+package com.asos.codingstyle.asoscodingstyle;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.microsoft.windowsazure.messaging.NotificationHub;
+import com.microsoft.windowsazure.notifications.NotificationsManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String SENDER_ID = "476143070248";
+    private GoogleCloudMessaging gcm;
+    private NotificationHub hub;
+    private final String HubName = "asoscodingstyle";
+    private final String HubListenConnectionString = "Endpoint=sb://asoscodingstyle-ns.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=AUa8kg3YJ7Q6L7sAUSJd0nxy9dqzcu0YMVUyX6TJViE=";
+    private static Boolean isVisible = false;
+    private final String orderUrl = "http://asos-coding-style-web.azurewebsites.net/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webview = new WebView(this);
+        setContentView(webview);
+
+        webview.getSettings().setJavaScriptEnabled(true);
+
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl(orderUrl);
+
+        MyHandler.mainActivity = this;
+        NotificationsManager.handleNotifications(this, SENDER_ID, MyHandler.class);
+        gcm = GoogleCloudMessaging.getInstance(this);
+        hub = new NotificationHub(HubName, HubListenConnectionString, this);
+        registerWithNotificationHubs();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void registerWithNotificationHubs() {
+        new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object... params) {
+                try {
+                    //String regid = gcm.register(SENDER_ID);
+                    gcm.register(SENDER_ID);
+                    //DialogNotify("Registered Successfully","RegId : " +
+                    //        hub.register(regid).getRegistrationId());
+                } catch (Exception e) {
+                    DialogNotify("Exception",e.getMessage());
+                    return e;
+                }
+                return null;
+            }
+        }.execute(null, null, null);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        isVisible = true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        isVisible = false;
+    }
+
+    /**
+     * A modal AlertDialog for displaying a message on the UI thread
+     * when there's an exception or message to report.
+     *
+     * @param title   Title for the AlertDialog box.
+     * @param message The message displayed for the AlertDialog box.
+     */
+    public void DialogNotify(final String title,final String message)
+    {
+        if (!isVisible)
+            return;
+
+        final AlertDialog.Builder dlg;
+        dlg = new AlertDialog.Builder(this);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog dlgAlert = dlg.create();
+                dlgAlert.setTitle(title);
+                dlgAlert.setButton(DialogInterface.BUTTON_POSITIVE,
+                        "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                dlgAlert.setMessage(message);
+                dlgAlert.setCancelable(false);
+                dlgAlert.show();
+            }
+        });
+    }
+}
