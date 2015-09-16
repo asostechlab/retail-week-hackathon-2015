1dcf067526df61560ab40e0c7d2b3095594ed004
Massimo Ahmadi <info@massimoahmadi.com>
2015-09-16 02:51:46 +0100

    Added drop-off point component

---
+++ AsosCodingStyle.Web/Scripts/apis/dropOffPointApi.ts
@@ -0,0 +1,52 @@
+﻿/// <reference path="../../../AsosCodingStyle.Data/Scripts/TypeLite.Net4.d.ts" />
+import $ = require('jquery');
+import Address = AsosCodingStyle.Data.Address;
+
+export interface IDropOffPoint {
+    Address: Address;
+    OpeningTimes: string[];
+    MapUrl: string;
+}
+
+export class DropOffPointApi {
+    getDropOffPoint(postCode: string): Promise<IDropOffPoint> {
+        var dropOffPoint: IDropOffPoint;
+
+        if (postCode === 'location') {
+            dropOffPoint = {
+                Address: {
+                    AddressLine1: 'H V Kingsley',
+                    AddressLine2: '5 Paul Street',
+                    City: 'London',
+                    PostCode: 'EC2A 4JJ'
+                },
+                OpeningTimes: [
+                    'Mon - Fri 06:00 - 18:00',
+                    'Sat closed',
+                    'Sun closed'
+                ],
+                MapUrl: '/images/ec2a4jj.PNG'
+            };
+        } else {
+            dropOffPoint = {
+                Address: {
+                    AddressLine1: 'Key Shop',
+                    AddressLine2: '227 Eversholt Street',
+                    City: 'London',
+                    PostCode: 'NW1 1DE'
+                },
+                OpeningTimes: [
+                    'Mon - Fri 07:00 - 19:30',
+                    'Sat 07:00 - 19:30',
+                    'Sun 07:00 - 19:30'
+                ],
+                MapUrl: '/images/nw11de.PNG'
+            };
+        }
+
+        return Promise.resolve(dropOffPoint);
+    }
+}
+
+var instance = new DropOffPointApi();
+export {instance};
--- AsosCodingStyle.Web/Scripts/components/dropOffPoint/dropOffPoint.ts
+++ AsosCodingStyle.Web/Scripts/components/dropOffPoint/dropOffPoint.ts
@@ -1,11 +1,37 @@
 ﻿import templateMarkup = require('text!./dropOffPoint.html');
 import ko = require('knockout');
+import DropOffPointApi = require("../../apis/dropOffPointApi");
 
 export class DropOffPoint {
 
     constructor() {
+        this.dropOffPointPostcode = ko.observable(null);
+        this.dropOffDetails = ko.observable(null);
+        this.isLocationReminderSelected = ko.observable(false);
+        this.isTimeReminderSelected = ko.observable(false);
+        this.isCalendarReminderSelected = ko.observable(false);
+        this.isEmailReminderSelected = ko.observable(false);
+        this.isMobileReminderSelected = ko.observable(false);
     }
 
+    postCodeKeyUp(d, e): void {
+        if (e.keyCode === 13) { // User has pressed enter
+            DropOffPointApi.instance.getDropOffPoint(this.dropOffPointPostcode()).then(dropOffPoint => this.dropOffDetails(dropOffPoint));
+        }
+    }
+
+    useMyLocation(d, e): void {
+        DropOffPointApi.instance.getDropOffPoint('location').then(dropOffPoint => this.dropOffDetails(dropOffPoint));
+        this.dropOffPointPostcode(null);
+    }
+
+    dropOffPointPostcode: KnockoutObservable<string>;
+    dropOffDetails: KnockoutObservable<DropOffPointApi.IDropOffPoint>;
+    isLocationReminderSelected: KnockoutObservable<boolean>;
+    isTimeReminderSelected: KnockoutObservable<boolean>;
+    isCalendarReminderSelected: KnockoutObservable<boolean>;
+    isEmailReminderSelected: KnockoutObservable<boolean>;
+    isMobileReminderSelected: KnockoutObservable<boolean>;
 }
 
 export {DropOffPoint as viewModel, templateMarkup as template};
