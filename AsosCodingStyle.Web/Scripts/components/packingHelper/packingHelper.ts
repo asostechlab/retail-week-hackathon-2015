3d6253e52baea270c7a053e2180a8d4f19407b00
Dylan Morley <dylanm@asos.com>
2015-09-16 03:35:56 +0100

    Added packing helper markup component

---
+++ AsosCodingStyle.Web/Scripts/components/packingHelper/packingHelper.ts
@@ -0,0 +1,11 @@
+﻿import templateMarkup = require('text!./packingHelper.html');
+import ko = require('knockout');
+
+export class Collection {
+
+    constructor() {
+    }
+
+}
+
+export {Collection as viewModel, templateMarkup as template};
