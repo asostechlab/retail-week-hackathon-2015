57b17abe30a8af8a63888413521fb227d261ddd0
Ian Kent <ianrkent@gmail.com>
2015-09-16 04:58:06 +0100

    implemented the saving

--- AsosCodingStyle.Web/Scripts/components/order/order.ts
+++ AsosCodingStyle.Web/Scripts/components/order/order.ts
@@ -6,6 +6,7 @@ export class Order {
 
     constructor() {
         this.hasLoaded = ko.observable(false);
+        this.hasSubmitted = ko.observable(false);
         const orderIdToLoad = "1"; // consider picking this up off the URL
         OrderModel.instance.retrieveOrder(orderIdToLoad).then(() => {
             this.orderItems = OrderModel.instance.order.Items;
@@ -16,6 +17,12 @@ export class Order {
         this.returnSummaryText = ko.pureComputed(this.computeReturnSummaryText, this);
     }
 
+    submitRefund(): void {
+        OrderModel.instance.saveOrder().then(() => {
+            this.hasSubmitted(true);
+        });
+    }
+
     private computeIsReturningItems(): boolean {
         return OrderModel.instance.orderItemsToReturn().length > 0;
     }
@@ -24,11 +31,12 @@ export class Order {
         var numberOfItemsBeingReturned = OrderModel.instance.orderItemsToReturn().length;
         return `Returning ${numberOfItemsBeingReturned} item${numberOfItemsBeingReturned > 1 ? 's' : ''}`;
     }
-    
+
     orderItems: AsosCodingStyle.Data.OrderItem[];
     isReturningItems: KnockoutComputed<boolean>;
     returnSummaryText: KnockoutComputed<string>;
     hasLoaded: KnockoutObservable<boolean>;
+    hasSubmitted: KnockoutObservable<boolean>;
 }
 
 export {Order as viewModel, templateMarkup as template};
