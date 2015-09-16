9ae4805762a025eb913cc4b8fde0f4f6dec76274
larrydavidformayor <lordscottframpton@outlook.com>
2015-09-16 01:04:40 +0100

    Data model changes

---
+++ AsosCodingStyle.Data/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypeLite;
+
+namespace AsosCodingStyle.Data
+{
+    [TsClass]
+    public class Address
+    {
+        public string AddressLine1 { get; set; }
+
+        public string AddressLine2 { get; set; }
+
+        public string City { get; set; }
+
+        public string PostCode { get; set; }
+    }
+}
