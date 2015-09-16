eef3034ed1f052eff6ef2d96a35576f52e0b6850
Massimo Ahmadi <info@massimoahmadi.com>
2015-09-16 12:12:20 +0100

    Minor mobile tweaks

--- AsosCodingStyle.Web/Scripts/components/packingDetails/packingDetails.ts
+++ AsosCodingStyle.Web/Scripts/components/packingDetails/packingDetails.ts
@@ -1,10 +1,12 @@
 ﻿import templateMarkup = require('text!./packingDetails.html');
 import ReturnsMethodModel = require("../../models/returnsMethodModel");
+import $ = require('jquery');
 
 export class PackingDetails {
 
     constructor() {
         this.isDropOffPointSelected = ReturnsMethodModel.instance.isDropOffPointSelected;
+        $('body').scrollTop(0);
     }
 
     isDropOffPointSelected: KnockoutComputed<boolean>;
